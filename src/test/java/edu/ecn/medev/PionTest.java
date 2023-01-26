6fa387b1e3a52b50e38f13e2a88e84511b9f0c7d
Mailys LE LOCH <mailys.le-loch@eleves.ec-nantes.fr>
2023-01-26 09:38:23 +0100

    ajout de la classe pion, ses classes filles et les TU de celles ci

---
+++ src/test/java/edu/ecn/medev/PionTest.java
@@ -0,0 +1,31 @@
+
+package edu.ecn.medev;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Cette classe est une classe de test et génère les tests unitaires pour la classe pion
+ * @author maily
+ */
+public class PionTest {
+    
+    /**
+     * Méthode test permettant de tester si la méthode getCouleur renvoie bien la couleur du pion
+     */
+    @org.junit.jupiter.api.Test
+    void getCouleur() {
+        Pion pion = new Pion("jaune");
+        assertEquals(pion.getCouleur(), "jaune");
+    }
+
+    /**
+     * Méthode test permettant de tester si la méthode setCouleur implémente bien la couleur que l'on souhaite dans pion
+     */
+    @org.junit.jupiter.api.Test
+    void setExemple() {
+        Pion pion = new Pion("bleu");
+        pion.setCouleur("bleu");
+        assertEquals(pion.getCouleur(), "bleu");
+    }
+    
+}
